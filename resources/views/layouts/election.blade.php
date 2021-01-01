b038ab4bc472678912e205a4b0f509bf58904af1
thelucasnorth <contact@lucasnorth.uk>
2021-01-01 21:25:00 +0000

    Add imprint display

--- resources/views/layouts/election.blade.php
+++ resources/views/layouts/election.blade.php
@@ -33,8 +33,16 @@
     <main>
         {{ $slot }}
     </main>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="p-2">
+                <div class="ml-2">
+                    {{request('election')->imprint}}
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-
 @stack('modals')
 
 @livewireScripts
