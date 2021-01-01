e21a5d226651bffe1671788bc55c5a762596a0f1
thelucasnorth <contact@lucasnorth.uk>
2021-01-01 23:03:35 +0000

    Add replace image label

--- resources/views/livewire/edit-candidate.blade.php
+++ resources/views/livewire/edit-candidate.blade.php
@@ -51,6 +51,9 @@
                                wire:model.lazy="removeImage">
                     </div>
                 </div>
+                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="candidateImage{{$uploadCount}}">
+                    Replace image
+                </label>
             @endif
             <input type="file" wire:model.lazy="image" id="candidateImage{{$uploadCount}}"
                    @error('image') class="border-red-500" @enderror>
