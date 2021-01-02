8fc5eb194952964d77bf1974f62e9bd161b21938
thelucasnorth <contact@lucasnorth.uk>
2021-01-02 13:10:51 +0000

    Implement result calculation using ResultController method

--- app/Http/Livewire/ViewResults.php
+++ app/Http/Livewire/ViewResults.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Livewire;
 
+use App\Http\Controllers\ResultController;
+use App\Models\Role;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class ViewResults extends Component
 {
+    protected $role;
+    protected $election;
+    public $results;
+
+    public function mount(\App\Models\Election $election, Role $role, $method) {
+        $this->election = $election;
+        $this->role = $role;
+
+        // this will use the defined method in ResultController. This can be changed as needed, swapped based on how electrr is being used, etc - calling the method here means that web and API returns are the same
+        $results = app(ResultController::class)->calculate($election, $role, $method);
+
+        $this->results = $results;
+    }
+
     public function render()
     {
         return view('livewire.view-results');
