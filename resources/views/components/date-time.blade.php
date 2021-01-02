e96de36d7b234fb337311bd00ef0652d39bfd3f1
thelucasnorth <contact@lucasnorth.uk>
2021-01-02 16:19:51 +0000

    Better organise datetime picker

--- resources/views/components/date-time.blade.php
+++ resources/views/components/date-time.blade.php
@@ -1,19 +1,21 @@
 <div>
 
-    @once
+    @push('scripts')
+        @once
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
         <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-
-    @endonce
+        @endonce
+    @endpush
 
     <input x-data x-ref="input" type="text" class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 dateTimePicker" {{$attributes}} >
 
+
+        @push('scripts')
     <script>
         flatpickr("#{{$attributes['id']}}", {
             enableTime: true,
-            dateFormat: "d-m-Y H:i",
-
+            dateFormat: "d-m-Y H:i"
         });
     </script>
-
+            @endpush
 </div>
