0eff4ba5cbb8555f6e67ce7b894f051400c8b1fb
thelucasnorth <contact@lucasnorth.uk>
2021-01-02 13:11:39 +0000

    Result calculation view

--- resources/views/dashboard/results.blade.php
+++ resources/views/dashboard/results.blade.php
@@ -14,7 +14,7 @@
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Create Election') }}
+            Results
         </h2>
     </x-slot>
 
@@ -24,20 +24,7 @@
 
                 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
 
-                    <div class="mt-8 text-2xl">
-                        Welcome to electrr!
-                    </div>
-
-                    <div class="mt-8">
-
-                        <div class="m-3">
-                            <button class="bg-white text-gray-800 font-bold rounded border-b-2 border-green-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"  width="24" height="24" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
-                                <span class="ml-2"> Create Election</span>
-                            </button>
-                        </div>
+                    <livewire:view-results :role="$role" :election="$election" :method="$method"/>
 
                     </div>
 
