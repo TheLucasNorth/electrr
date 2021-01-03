e91ea6e3f3b21556c557e331ed6cf26dc4281e34
thelucasnorth <contact@lucasnorth.uk>
2021-01-03 14:33:03 +0000

    Add casts on Voter.php

--- app/Models/Voter.php
+++ app/Models/Voter.php
@@ -12,29 +12,33 @@ class Voter extends Authenticatable
     use SoftDeletes;
     use HasFactory;
 
-    public function election() {
+    public function election()
+    {
         return $this->belongsTo('App\Models\Election');
     }
 
-    public function username() {
+    public function username()
+    {
         $string[1] = Str::random(3);
         $string[2] = Str::random(3);
         $string[3] = Str::random(3);
         return Str::upper(implode('-', $string));
     }
 
-    public function safeUsername($election) {
+    public function safeUsername($election)
+    {
         $dupe = true;
         while ($dupe) {
             $username = $this->username();
-            if(!Voter::where('election_id', $election)->where('username', $username)->exists()) {
+            if (!Voter::where('election_id', $election)->where('username', $username)->exists()) {
                 $dupe = false;
             }
         }
         return $username;
     }
 
-    public function password() {
+    public function password()
+    {
         return Str::upper(Str::random(6));
     }
 
@@ -42,5 +46,15 @@ class Voter extends Authenticatable
 
     protected $hidden = [
         'password'
+    ];
+
+    protected $casts = [
+        'voted' => 'boolean',
+        'delivered' => 'boolean',
+        'opened' => 'boolean',
+        'unsubscribed' => 'boolean',
+        'complained' => 'boolean',
+        'bounced' => 'boolean',
+        'election_id' => 'integer'
     ];
 }
