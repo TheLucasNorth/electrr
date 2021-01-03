5dba0e2b1c11ae1e219173111d51d7896c7790da
thelucasnorth <contact@lucasnorth.uk>
2021-01-03 00:11:53 +0000

    Add scaffolded OpaVote API method

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -32,7 +32,7 @@ class ResultController extends Controller
         foreach ($candidates as $candidate) {
             $useableCandidates[] = $candidate->name;
             if ($candidate->withdrawn) {
-                $withdrawnCandidates[] = '-'.array_key_last($useableCandidates);
+                $withdrawnCandidates[] = '-' . array_key_last($useableCandidates);
             }
         }
         unset($useableCandidates[0]);
@@ -41,7 +41,7 @@ class ResultController extends Controller
         // At the same time, an array is formed for all withdrawn candidates. This array will hold the key for that candidate, as determined by that candidates key in the useableCandidates array, preceded by a minus sign.
         $output = [];
         // begin crafting the output
-        $output[] = count($useableCandidates).' '.$role->seats.' # Number of candidates and number of seats to be elected';
+        $output[] = count($useableCandidates) . ' ' . $role->seats . ' # Number of candidates and number of seats to be elected';
         $output[] = "# Withdrawn candidates. If none are listed, no candidates were withdrawn at the time of counting results.";
         $output[] = implode(' ', $withdrawnCandidates);
         $output[] = "# Votes, expressed as a 1 to start each line, the candidates in preference order, and a 0 to end each line.";
@@ -67,12 +67,13 @@ class ResultController extends Controller
      * @return \Symfony\Component\HttpFoundation\StreamedResponse
      * Returns the blt format result as a downloadable file
      */
-    public function download(Election $election, Role $role) {
+    public function download(Election $election, Role $role)
+    {
         $this->election = $election;
         $this->role = $role;
         return response()->streamDownload(function () {
             echo $this->generate($this->election, $this->role);
-        }, $election->name.' - '.$role->name.'.blt');
+        }, $election->name . ' - ' . $role->name . '.blt');
     }
 
     /**
@@ -81,7 +82,8 @@ class ResultController extends Controller
      * @param $method
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
-    public function display(Election $election, Role $role, $method) {
+    public function display(Election $election, Role $role, $method)
+    {
         return view('dashboard.results')->with('election', $election)->with('role', $role)->with('method', $method);
     }
 
@@ -91,7 +93,8 @@ class ResultController extends Controller
      * @param Role $role
      * @return \Illuminate\Http\JsonResponse|string
      */
-    public function apiGenerate(Request $request, Election $election, Role $role) {
+    public function apiGenerate(Request $request, Election $election, Role $role)
+    {
         if (!$request->user()->tokenCan('results')) {
             return response()->json(['message' => 'not authorised'], 401);
         }
@@ -104,7 +107,8 @@ class ResultController extends Controller
      * @param Role $role
      * @return \Illuminate\Http\JsonResponse|string
      */
-    public function apiCalculate(Request $request, Election $election, Role $role, $method) {
+    public function apiCalculate(Request $request, Election $election, Role $role, $method)
+    {
         if (!$request->user()->tokenCan('results')) {
             return response()->json(['message' => 'not authorised'], 401);
         }
@@ -156,4 +160,78 @@ class ResultController extends Controller
     }
     */
 
+    /**
+     * OpaVote method - use OpaVote to count results
+     * Please note that extra configuration is needed to use this method
+     * @param Election $election
+     * @param Role $role
+     * @param $method
+     * @return string
+     */
+    /*
+    public function calculate(Election $election, Role $role, $method)
+    {
+        $method = $this->changeMethod($method);
+        $blt = $this->generate($election, $role);
+        $client = new Client();
+        $endpoint = "https://www.opavote.com/api/v1/counts";
+        $api = "YOUR OPAVOTE API KEY";
+        $request = array("method" => $method, "blt" => $blt);
+        $response = $client->request('POST', $endpoint, [
+            'headers' => [
+                'x-api-key' => $api,
+                'Content-Type' => 'application/json'
+            ],
+            'body' => json_encode($request)
+        ]);
+        return $response->getBody()->getContents();
+    }
+
+    public function changeMethod($method)
+    {
+        if ($method === "Approval") {
+            return "Approval Voting";
+        }
+        if ($method === "Borda") {
+            return "Borda Count";
+        }
+        if ($method === "Bucklin") {
+            return "Bucklin System";
+        }
+        if ($method === "CambridgeSTV") {
+            return "Cambridge STV (Massachusetts, USA)";
+        }
+        if ($method === "Coombs") {
+            return "Coombs Method";
+        }
+        if ($method === "ERS97STV") {
+            return "ERS97 STV";
+        }
+        if ($method === "GPCA2000STV") {
+            return "Green Party of California STV";
+        }
+        if ($method === "IRV") {
+            return "Instant Runoff Voting";
+        }
+        if ($method === "MeekSTV") {
+            return "Meek STV";
+        }
+        if ($method === "MinneapolisSTV") {
+            return "Minneapolis STV";
+        }
+        if ($method === "NIrelandSTV") {
+            return "N. Ireland STV";
+        }
+        if ($method === "SanFranciscoRCV") {
+            return "San Francisco RCV";
+        }
+        if ($method === "ScottishSTV") {
+            return "Scottish STV";
+        }
+        if ($method === "WarrenSTV") {
+            return "Warren STV";
+        }
+    }
+    */
 }
+
